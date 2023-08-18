d16c83b059b2130d56a6fed15090fc0b72b02ea5
jessexmaki <jesse.maki@ufl.edu>
2023-08-18 18:08:24 -0400

    upload

--- data_calculating_and_classification.py
+++ data_calculating_and_classification.py
@@ -102,3 +102,134 @@ df_final.to_csv('Data/aromatase_04_bioactivity_data_3class_pIC50.csv')
 # Filter out intermediate class and save the 2-class dataset
 df_2class = df_final[df_final['class'] != 'intermediate']
 df_2class.to_csv('Data/aromatase_05_bioactivity_data_2class_pIC50.csv')
+
+# Importing necessary libraries
+import seaborn as sns
+sns.set(style='ticks')
+import matplotlib.pyplot as plt
+
+# Set the default size of all plots
+plt.figure(figsize=(5.5, 5.5))
+
+# Plotting a bar chart for frequency of each bioactivity class
+sns.countplot(x='class', data=df_2class, edgecolor='black')
+
+# Adding labels for the x and y axes
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('Frequency', fontsize=14, fontweight='bold')
+
+# Saving the plot as a PDF
+plt.savefig('Data/plot_bioactivity_class.pdf')
+
+# Set the default size for the next plot
+plt.figure(figsize=(5.5, 5.5))
+
+# Scatter plot of MW vs LogP, colored by class and sized by pIC50
+sns.scatterplot(x='MW', y='LogP', data=df_2class, hue='class', size='pIC50', edgecolor='black', alpha=0.7)
+
+# Adding labels for the x and y axes
+plt.xlabel('MW', fontsize=14, fontweight='bold')
+plt.ylabel('LogP', fontsize=14, fontweight='bold')
+
+# Adding a legend to the plot
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0)
+
+# Set the default size for the next plot
+plt.figure(figsize=(5.5, 5.5))
+
+# Boxplot for pIC50 values across bioactivity classes
+sns.boxplot(x = 'class', y = 'pIC50', data = df_2class)
+
+# Adding labels for the x and y axes
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('pIC50 value', fontsize=14, fontweight='bold')
+
+# Saving the plot as a PDF
+plt.savefig('Data/plot_ic50.pdf')
+
+
+# Define a function for performing the Mann-Whitney U test
+def mannwhitney(descriptor, verbose=False):
+    # Import necessary libraries and modules
+    from numpy.random import seed
+    from numpy.random import randn
+    from scipy.stats import mannwhitneyu
+
+    # Seed the random number generator for reproducibility
+    seed(1)
+
+    # Extract active samples for the given descriptor
+    selection = [descriptor, 'class']
+    df = df_2class[selection]
+    active = df[df['class'] == 'active']
+    active = active[descriptor]
+
+    # Extract inactive samples for the given descriptor
+    selection = [descriptor, 'class']
+    df = df_2class[selection]
+    inactive = df[df['class'] == 'inactive']
+    inactive = inactive[descriptor]
+
+    # Perform the Mann-Whitney U test
+    stat, p = mannwhitneyu(active, inactive)
+
+    # Interpret the result
+    alpha = 0.05
+    if p > alpha:
+        interpretation = 'Same distribution (fail to reject H0)'
+    else:
+        interpretation = 'Different distribution (reject H0)'
+
+    # Compile the results into a DataFrame
+    results = pd.DataFrame({'Descriptor':descriptor,
+                            'Statistics':stat,
+                            'p':p,
+                            'alpha':alpha,
+                            'Interpretation':interpretation}, index=[0])
+    
+    # Save the results as a CSV file
+    filename = 'Data/mannwhitneyu_' + descriptor + '.csv'
+    results.to_csv(filename)
+
+    return results
+
+# Run the Mann-Whitney U test for pIC50 descriptor
+mannwhitney('pIC50')
+
+# Plotting boxplots for other descriptors as well, following the same pattern
+plt.figure(figsize=(5.5, 5.5))
+sns.boxplot(x = 'class', y = 'MW', data = df_2class)
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('MW', fontsize=14, fontweight='bold')
+plt.savefig('Data/plot_MW.pdf')
+mannwhitney('MW')
+
+plt.figure(figsize=(5.5, 5.5))
+sns.boxplot(x = 'class', y = 'MW', data = df_2class)
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('MW', fontsize=14, fontweight='bold')
+plt.savefig('Data/plot_MW.pdf')
+mannwhitney('LogP')
+
+plt.figure(figsize=(5.5, 5.5))
+sns.boxplot(x = 'class', y = 'LogP', data = df_2class)
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('LogP', fontsize=14, fontweight='bold')
+plt.savefig('Data/plot_LogP.pdf')
+mannwhitney('NumHDonors')
+
+plt.figure(figsize=(5.5, 5.5))
+sns.boxplot(x = 'class', y = 'NumHDonors', data = df_2class)
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('NumHDonors', fontsize=14, fontweight='bold')
+plt.savefig('Data/plot_NumHDonors.pdf')
+
+plt.figure(figsize=(5.5, 5.5))
+sns.boxplot(x = 'class', y = 'NumHAcceptors', data = df_2class)
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('NumHAcceptors', fontsize=14, fontweight='bold')
+plt.savefig('Data/plot_NumHAcceptors.pdf')
+mannwhitney('NumHAcceptors')
+
+# Zip the resulting CSV and PDF files
+# ! zip -r results.zip . -i *.csv *.pdf
